221486b4c61b4099b86993d5ede4f46fbda76b23
Eric Rozell <erozell@outlook.com>
2019-07-05 00:18:05 -0400

    Adds project to support LUIS v3
    In order to keep things stable in the near term, we will maintain two versions of the LUIS NLU service provider, one for LUIS v2 and the other for LUIS v3. For the most part, the LUIS v3 library is a copy paste of LUIS v2, and some minor modifications to work with the latest API changes.
    
    The LUIS v3 library supports the new runtime API, including the ability to use "query" instead of "text" for the test utterance, and the ability to add extra JSON properties to your request, such as "externalEntities", "options" and "dynamicLists"
    
    There is a remaining TODO to figure out what to do with entity value resolution, as the entity values in LUIS v3 follow a less consistent pattern than in v2. Follow issue #27 for progress on this issue.
    
    Fixes #39

---
+++ src/NLU.DevOps.LuisV3.Tests/GlobalSuppressions.cs
@@ -0,0 +1,4 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1806:Do not ignore method results", Justification = "Creating the object results in a tested exception.", Scope = "member", Target = "~M:NLU.DevOps.Luis.Tests.LuisV3NLUServiceTests.ThrowsArgumentNull")]
