e1b72920b533fd4a58d815b797017dce3af45f6a
Elizabeth Graham <esenoch@hotmail.com>
2019-07-19 14:56:05 -0400

    Apply all parallelism changes and unit tests.

--- src/NLU.DevOps.CommandLine/EnumerableExtensions.cs
+++ src/NLU.DevOps.CommandLine/EnumerableExtensions.cs
@@ -10,13 +10,11 @@ namespace NLU.DevOps.CommandLine
 
     internal static class EnumerableExtensions
     {
-        private const int DegreeOfParallelism = 3;
-
-        public static async Task<IEnumerable<TResult>> SelectAsync<T, TResult>(this IEnumerable<T> items, Func<T, Task<TResult>> selector)
+        public static async Task<IEnumerable<TResult>> SelectAsync<T, TResult>(this IEnumerable<T> items, Func<T, Task<TResult>> selector, int degreeOfParallelism)
         {
             var indexedItems = items.Select((item, i) => new { Item = item, Index = i });
             var results = new TResult[items.Count()];
-            var tasks = new List<Task<Tuple<int, TResult>>>(DegreeOfParallelism);
+            var tasks = new List<Task<Tuple<int, TResult>>>(degreeOfParallelism);
 
             async Task<Tuple<int, TResult>> selectWithIndexAsync(T item, int i)
             {
@@ -26,7 +24,7 @@ namespace NLU.DevOps.CommandLine
 
             foreach (var indexedItem in indexedItems)
             {
-                if (tasks.Count == DegreeOfParallelism)
+                if (tasks.Count == degreeOfParallelism)
                 {
                     var task = await Task.WhenAny(tasks).ConfigureAwait(false);
                     tasks.Remove(task);
