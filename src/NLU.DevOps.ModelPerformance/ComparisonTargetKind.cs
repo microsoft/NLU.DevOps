80562d9108aac5fb8623a9bfbaaddf70fb320632
Eric Rozell <erozell@outlook.com>
2019-07-20 07:05:21 -0400

    Adds metadata to generalize results from NLU.DevOps.ModelPerformance
    In order to support a visualization of NLU confusion matrix results, we should support outputting a generalized form of the test cases that can be easily consumed by other tools.

---
+++ src/NLU.DevOps.ModelPerformance/ComparisonTargetKind.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.ModelPerformance
+{
+    /// <summary>
+    /// Comparison target kind.
+    /// </summary>
+    public enum ComparisonTargetKind
+    {
+        /// <summary>
+        /// Text.
+        /// </summary>
+        Text,
+
+        /// <summary>
+        /// Intent.
+        /// </summary>
+        Intent,
+
+        /// <summary>
+        /// Entity.
+        /// </summary>
+        Entity,
+
+        /// <summary>
+        /// Entity value.
+        /// </summary>
+        EntityValue,
+
+        /// <summary>
+        /// Entity resolution.
+        /// </summary>
+        EntityResolution
+    }
+}
---
+++ src/NLU.DevOps.ModelPerformance/ConfusionMatrixResultKind.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.ModelPerformance
+{
+    /// <summary>
+    /// Enumeration of confusion matrix result types.
+    /// </summary>
+    public enum ConfusionMatrixResultKind
+    {
+        /// <summary>
+        /// True positive.
+        /// </summary>
+        TruePositive,
+
+        /// <summary>
+        /// True negative.
+        /// </summary>
+        TrueNegative,
+
+        /// <summary>
+        /// False positive.
+        /// </summary>
+        FalsePositive,
+
+        /// <summary>
+        /// False negative.
+        /// </summary>
+        FalseNegative,
+    }
+}
