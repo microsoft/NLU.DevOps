26e985a051263a00496238fa5bc2a85b63bb61bd
Eric Rozell <erozell@outlook.com>
2019-07-24 14:08:55 -0400

    Adds INLUTestClient implementation for DialogFlow
    Uses the Google.Cloud.DialogFlow.V2 NuGet package to test utterances for DialogFlow.

---
+++ src/NLU.DevOps.DialogFlow/DialogFlowNLUClientFactory.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.DialogFlow
+{
+    using System;
+    using System.Composition;
+    using Microsoft.Extensions.Configuration;
+    using Models;
+
+    /// <summary>
+    /// DialogFlow NLU client factory.
+    /// </summary>
+    [Export("dialogflow", typeof(INLUClientFactory))]
+    public class DialogFlowNLUClientFactory : INLUClientFactory
+    {
+        /// <inheritdoc />
+        public INLUTestClient CreateTestInstance(IConfiguration configuration, string settingsPath)
+        {
+            return new DialogFlowNLUTestClient(configuration ?? throw new ArgumentNullException(nameof(configuration)));
+        }
+
+        /// <inheritdoc />
+        public INLUTrainClient CreateTrainInstance(IConfiguration configuration, string settingsPath)
+        {
+            throw new NotSupportedException("The DialogFlow NLU.DevOps package does not currently support training.");
+        }
+    }
+}
---
+++ src/NLU.DevOps.DialogFlow/DialogFlowNLUTestClient.cs
@@ -0,0 +1,167 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.DialogFlow
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Core;
+    using Google.Apis.Auth.OAuth2;
+    using Google.Cloud.Dialogflow.V2;
+    using Google.Protobuf;
+    using Google.Protobuf.WellKnownTypes;
+    using Grpc.Auth;
+    using Microsoft.Extensions.Configuration;
+    using Models;
+    using Newtonsoft.Json.Linq;
+
+    internal sealed class DialogFlowNLUTestClient : DefaultNLUTestClient
+    {
+        private const string DialogFlowClientKeyJsonConfigurationKey = "dialogflowClientKeyJson";
+        private const string DialogFlowProjectIdConfigurationKey = "dialogflowProjectId";
+        private const string DialogFlowSessionIdConfigurationKey = "dialogflowSessionId";
+
+        private SessionsClient sessionsClient;
+
+        public DialogFlowNLUTestClient(SessionsClient sessionsClient, IConfiguration configuration)
+            : this(configuration)
+        {
+            this.sessionsClient = sessionsClient;
+        }
+
+        public DialogFlowNLUTestClient(IConfiguration configuration)
+        {
+            this.Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        private IConfiguration Configuration { get; }
+
+        private string ProjectId => this.GetConfigurationValue(DialogFlowProjectIdConfigurationKey);
+
+        private string SessionId => this.GetConfigurationValue(DialogFlowSessionIdConfigurationKey, true);
+
+        protected override async Task<LabeledUtterance> TestAsync(string utterance, CancellationToken cancellationToken)
+        {
+            var sessionId = this.SessionId ?? Guid.NewGuid().ToString();
+            var sessionName = new SessionName(this.ProjectId, sessionId);
+            var queryInput = new QueryInput
+            {
+                Text = new TextInput
+                {
+                    Text = utterance,
+                    LanguageCode = "en",
+                }
+            };
+
+            var client = await this.GetSessionClientAsync(cancellationToken).ConfigureAwait(false);
+            var result = await client.DetectIntentAsync(sessionName, queryInput, cancellationToken).ConfigureAwait(false);
+            return new LabeledUtterance(
+                result.QueryResult.QueryText,
+                result.QueryResult.Intent.DisplayName,
+                result.QueryResult.Parameters?.Fields.Select(GetEntity).OfType<Entity>().ToList());
+        }
+
+        protected override async Task<LabeledUtterance> TestSpeechAsync(string speechFile, CancellationToken cancellationToken)
+        {
+            var sessionId = this.SessionId ?? Guid.NewGuid().ToString();
+            var sessionName = new SessionName(this.ProjectId, sessionId);
+            var byteString = await ByteString.FromStreamAsync(File.OpenRead(speechFile), cancellationToken).ConfigureAwait(false);
+            var request = new DetectIntentRequest
+            {
+                InputAudio = byteString,
+                SessionAsSessionName = sessionName,
+                QueryInput = new QueryInput
+                {
+                    AudioConfig = new InputAudioConfig
+                    {
+                        AudioEncoding = AudioEncoding.Unspecified,
+                        LanguageCode = "en",
+                        SampleRateHertz = 16000,
+                    },
+                },
+            };
+
+            var client = await this.GetSessionClientAsync(cancellationToken).ConfigureAwait(false);
+            var result = await client.DetectIntentAsync(request, cancellationToken).ConfigureAwait(false);
+            return new LabeledUtterance(
+                result.QueryResult.QueryText,
+                result.QueryResult.Intent.DisplayName,
+                result.QueryResult.Parameters?.Fields.Select(GetEntity).OfType<Entity>().ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            // no-op
+        }
+
+        private static Entity GetEntity(KeyValuePair<string, Value> pair)
+        {
+            JToken toJson(Value value)
+            {
+                switch (value.KindCase)
+                {
+                    case Value.KindOneofCase.BoolValue:
+                        return value.BoolValue;
+                    case Value.KindOneofCase.NumberValue:
+                        return value.NumberValue;
+                    case Value.KindOneofCase.StringValue:
+                        return string.IsNullOrEmpty(value.StringValue)
+                            ? default(JToken)
+                            : value.StringValue;
+                    case Value.KindOneofCase.ListValue:
+                        return new JArray(value.ListValue.Values.Select(toJson).ToArray());
+                    case Value.KindOneofCase.StructValue:
+                        var jsonObject = new JObject();
+                        foreach (var field in value.StructValue.Fields)
+                        {
+                            jsonObject.Add(field.Key, toJson(field.Value));
+                        }
+
+                        return jsonObject;
+                    default:
+                        return null;
+                }
+            }
+
+            var jsonValue = toJson(pair.Value);
+            if (jsonValue == null)
+            {
+                return null;
+            }
+
+            return new Entity(pair.Key, jsonValue, null, null, 0);
+        }
+
+        private async Task<SessionsClient> GetSessionClientAsync(CancellationToken cancellationToken)
+        {
+            if (this.sessionsClient != null)
+            {
+                return this.sessionsClient;
+            }
+
+            var keyJson = this.GetConfigurationValue(DialogFlowClientKeyJsonConfigurationKey);
+            var googleCredential = GoogleCredential.FromJson(keyJson);
+            var builder = new SessionsClientBuilder
+            {
+                ChannelCredentials = googleCredential.ToChannelCredentials(),
+            };
+
+            return this.sessionsClient = await builder.BuildAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        private string GetConfigurationValue(string key, bool optional = false)
+        {
+            var value = this.Configuration[key];
+            if (value == null && !optional)
+            {
+                throw new InvalidOperationException($"Configuration value for '{key}' must be set.");
+            }
+
+            return value;
+        }
+    }
+}
