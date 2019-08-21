54cd9566b65002f429bf341f5cec65716082bbe7
Eric Rozell <erozell@outlook.com>
2019-08-20 22:14:36 -0400

    Adds mock provider implementation for use in samples and CI builds
    This PR adds a mock provider to the repo so we can avoid using LUIS for end-to-end tests running in CI.
    
    Fixes #127

---
+++ src/NLU.DevOps.MockProvider/MockNLUClient.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.MockProvider
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Core;
+    using Models;
+    using Newtonsoft.Json;
+
+    internal class MockNLUClient : DefaultNLUTestClient, INLUTrainClient
+    {
+        public MockNLUClient(string trainedUtterances)
+        {
+            this.Utterances = new List<LabeledUtterance>();
+            if (trainedUtterances != null)
+            {
+                this.Utterances.AddRange(
+                    JsonConvert.DeserializeObject<IEnumerable<LabeledUtterance>>(trainedUtterances));
+            }
+        }
+
+        public string TrainedUtterances => JsonConvert.SerializeObject(this.Utterances);
+
+        private List<LabeledUtterance> Utterances { get; }
+
+        public Task TrainAsync(IEnumerable<LabeledUtterance> utterances, CancellationToken cancellationToken)
+        {
+            this.Utterances.AddRange(utterances);
+            return Task.CompletedTask;
+        }
+
+        public Task CleanupAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+
+        protected override Task<LabeledUtterance> TestAsync(string utterance, CancellationToken cancellationToken)
+        {
+            var matchedUtterance = this.Utterances.FirstOrDefault(u => u.Text == utterance);
+            return Task.FromResult(matchedUtterance ?? new LabeledUtterance(null, null, null));
+        }
+
+        protected override Task<LabeledUtterance> TestSpeechAsync(string speechFile, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+        }
+    }
+}
---
+++ src/NLU.DevOps.MockProvider/MockNLUClientFactory.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.MockProvider
+{
+    using System.Composition;
+    using Microsoft.Extensions.Configuration;
+    using Models;
+
+    [Export("mock", typeof(INLUClientFactory))]
+    internal class MockNLUClientFactory : INLUClientFactory
+    {
+        public INLUTrainClient CreateTrainInstance(IConfiguration configuration, string settingsPath)
+        {
+            return new MockNLUClient(configuration["trainedUtterances"]);
+        }
+
+        public INLUTestClient CreateTestInstance(IConfiguration configuration, string settingsPath)
+        {
+            return new MockNLUClient(configuration["trainedUtterances"]);
+        }
+    }
+}
