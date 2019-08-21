5c40c0b83af05dc46a4440514bcd67a9a6bb7e12
Eric Rozell <erozell@outlook.com>
2019-08-20 22:14:36 -0400

    Adds mock provider implementation for use in samples and CI builds
    This PR adds a mock provider to the repo so we can avoid using LUIS for end-to-end tests running in CI.
    
    Fixes #127

---
+++ src/NLU.DevOps.MockProvider/Program.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.MockProvider
+{
+    using System;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
