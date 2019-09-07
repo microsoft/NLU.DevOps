1c5ef50634b3e32601c890e3e912b00d65ce32d2
Eric Rozell <erozell@outlook.com>
2019-09-07 10:29:33 -0400

    Reduces verbosity of confusion matrix JSON
    Rather than using a JSON object like:
    ```json
    {
      "truePositive": 42,
      "trueNegative": 7,
      "falsePositive": 1,
      "falseNegative": 0
    }
    ```
    
    This change uses a JSON array for a concise representation of confusion matrices, i.e., `[ 42, 7, 1, 0 ]`.
    
    Fixes #159

---
+++ src/NLU.DevOps.ModelPerformance.Tests/ConfusionMatrixConverterTests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.ModelPerformance.Tests
+{
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal static class ConfusionMatrixConverterTests
+    {
+        [Test]
+        public static void WritesNullValue()
+        {
+            var converter = new ConfusionMatrixConverter();
+            var serializer = JsonSerializer.CreateDefault();
+            serializer.Converters.Add(converter);
+            var json = JToken.FromObject(new ConfusionMatrixContainer(), serializer);
+            json.Type.Should().Be(JTokenType.Object);
+            json.As<JObject>().ContainsKey("Data").Should().BeTrue();
+            json["Data"].Type.Should().Be(JTokenType.Null);
+        }
+
+        [Test]
+        public static void WritesValueAsArray()
+        {
+            var value = new ConfusionMatrix(42, 7, 1, 0);
+            var converter = new ConfusionMatrixConverter();
+            var serializer = JsonSerializer.CreateDefault();
+            serializer.Converters.Add(converter);
+            var json = JToken.FromObject(value, serializer);
+            json.Type.Should().Be(JTokenType.Array);
+            json.As<JArray>().Count.Should().Be(4);
+            json.Value<int>(0).Should().Be(42);
+            json.Value<int>(1).Should().Be(7);
+            json.Value<int>(2).Should().Be(1);
+            json.Value<int>(3).Should().Be(0);
+        }
+
+        private class ConfusionMatrixContainer
+        {
+            public ConfusionMatrix Data { get; set; }
+        }
+    }
+}
