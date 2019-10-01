90ce84f28328fccc2ec59970e64dbae8d41a73d0
Eric Rozell <erozell@outlook.com>
2019-10-01 10:12:20 -0400

    Fixes support for LUIS batch test format
    LUIS batch uses `entity` instead of `entityType` for the type of entity matched. This change updates the Json.NET converter to also look for `entity` in cases where `entityType` is missing.
    
    Fixes #191

--- src/NLU.DevOps.Core/LabeledUtteranceConverter.cs
+++ src/NLU.DevOps.Core/LabeledUtteranceConverter.cs
@@ -9,7 +9,7 @@ namespace NLU.DevOps.Core
     using Newtonsoft.Json.Linq;
 
     /// <summary>
-    /// JSON converter for <see cref="LabeledUtterance"/>.
+    /// JSON converter for <see cref="LabeledUtterance"/> to recognize LUIS batch test format.
     /// </summary>
     public class LabeledUtteranceConverter : JsonConverter<LabeledUtterance>
     {
@@ -95,6 +95,14 @@ namespace NLU.DevOps.Core
                     jsonObject.Add("matchIndex", matchIndex);
                     jsonObject.Remove("startPos");
                     jsonObject.Remove("endPos");
+                }
+
+                var entity = jsonObject.Value<string>("entity");
+                var entityType = jsonObject.Value<string>("entityType");
+                if (entityType == null && entity != null)
+                {
+                    jsonObject.Add("entityType", entity);
+                    jsonObject.Remove("entity");
                 }
 
                 serializer.Converters.Remove(this);
