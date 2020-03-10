75f84ccb617a19fd439663a4ceeddea83dc8a164
Eric Rozell <erozell@outlook.com>
2020-03-10 16:58:20 -0400

    Fixes minor issue with configuration parsing (#276)
    Previously, we expected globally ignored or strict entity type configuration to be serialized. This change supports JSON (not serialized).

---
+++ src/NLU.DevOps.ModelPerformance.Tests/TestSettingsTests.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace NLU.DevOps.ModelPerformance.Tests
+{
+    using System;
+    using System.IO;
+    using FluentAssertions;
+    using Microsoft.Extensions.Configuration;
+    using Newtonsoft.Json.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal static class TestSettingsTests
+    {
+        [Test]
+        public static void ThrowsArgumentNull()
+        {
+            Func<bool> nullConfiguration = () => new TestSettings(null).Strict;
+            nullConfiguration.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("configuration");
+        }
+
+        [Test]
+        public static void CreateFromJson()
+        {
+            var settingsFile = Guid.NewGuid().ToString();
+
+            var settings = new JObject
+            {
+                { "trueNegativeIntent", "default" },
+                { "strictEntities", new JArray { "strict" } },
+                { "ignoreEntities", new JArray { "ignore" } },
+            };
+
+            File.WriteAllText(settingsFile, settings.ToString());
+
+            try
+            {
+                var configuration = new ConfigurationBuilder()
+                    .AddJsonFile(settingsFile)
+                    .Build();
+
+                var testSettings = new TestSettings(configuration);
+                testSettings.TrueNegativeIntent.Should().Be("default");
+                testSettings.StrictEntities.Should().BeEquivalentTo("strict");
+                testSettings.IgnoreEntities.Should().BeEquivalentTo("ignore");
+            }
+            finally
+            {
+                File.Delete(settingsFile);
+            }
+        }
+    }
+}
