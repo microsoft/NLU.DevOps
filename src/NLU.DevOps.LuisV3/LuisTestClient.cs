b71edcfd7ad62f1a1a8f3e4e2391042421f58a01
Eric Rozell <erozell@outlook.com>
2020-03-16 12:08:37 -0400

    Disables logging from test command (#289)
    When running batch predictions, this change sets the log parameter to false for LUIS so the tests are not reported in the query logs for the endpoint.
    
    Fixes #288

--- src/NLU.DevOps.LuisV3/LuisTestClient.cs
+++ src/NLU.DevOps.LuisV3/LuisTestClient.cs
@@ -56,6 +56,7 @@ namespace NLU.DevOps.Luis
                                 this.LuisConfiguration.VersionId,
                                 predictionRequest,
                                 verbose: true,
+                                log: false,
                                 cancellationToken: cancellationToken)
                             .ConfigureAwait(false);
                     }
@@ -65,6 +66,7 @@ namespace NLU.DevOps.Luis
                             this.LuisConfiguration.SlotName,
                             predictionRequest,
                             verbose: true,
+                            log: false,
                             cancellationToken: cancellationToken)
                         .ConfigureAwait(false);
                 }
